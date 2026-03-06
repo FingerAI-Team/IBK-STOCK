c70f54c0656dbbc1953e676b2079000d8f277018
Finger-cori <badzoo@naver.com>
2026-03-06 17:28:02 +0900

    debug

--- src/pipes/classify_pipe.py
+++ src/pipes/classify_pipe.py
@@ -35,16 +35,16 @@ class DataClassifier:
                 if not text:
                     continue
                 stock_pred = self.predictor_env.predict(text)
-                print(f'stock_pred: {stock_pred}, text: {text}')
                 fast_filter = self.filter_text(text)
-                print('fast_filter: ', fast_filter)
                 if fast_filter is not None:
                     stock_pred = fast_filter
 
                 enc_res = 'o' if stock_pred == 'stock' else 'x'
                 conv_id = record["conv_id"]
                 cls_rows.append((conv_id, enc_res))
+                print('ok step 1')
             except: 
+                print(f"Error processing record with conv_id {record.get('conv_id')}")
                 print(text)
         print(f'cls_rows: {cls_rows}')
         return cls_rows 
